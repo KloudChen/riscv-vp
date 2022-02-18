df8dd67592a2e25f3811b323e61384ca7b3e24cb
KloudChen <wei.c.chen@intel.com>
2022-02-18 10:55:25 +0800

    pass build

--- vp/cmake/Modules/FindDut.cmake
+++ vp/cmake/Modules/FindDut.cmake
@@ -1,4 +1,4 @@
-set(DUT_DIR         //home/kloud/cosim/riscv-vp/vp/src/platform/axi/dut)
+set(DUT_DIR         /home/kloud/cosim/riscv-vp/vp/src/platform/axi/dut)
 set(DUT_NAME        StringTransformer)
 set(DUT_SRC_V       ${DUT_DIR}/${DUT_NAME}.v)
 set(DUT_SC_DIR      ${DUT_DIR}/obj_dir) # generated systemc module dir
--- vp/src/platform/axi/dut/cmake/Modules/FindSystemC.cmake
+++
@@ -1,3 +0,0 @@
-set(SYSTEMC_INCLUDE_DIRS /usr/include)
-set(SYSTEMC_INCLUDE_DIR  ${SYSTEMC_INCLUDE_DIRS})
-set(SYSTEMC_LIBRARIES    /usr/lib/x86_64-linux-gnu/libsystemc.so)
